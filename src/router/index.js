f882d5173489954d82720676862a5ae942da35f8
ryunakae <ryunakae@gmail.com>
2021-02-12 00:09:59 +0900

    initial commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import BootstrapVue from 'bootstrap-vue'
 
 Vue.use(VueRouter)
+Vue.use(BootstrapVue)
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 const routes = [
   {
@@ -10,14 +15,6 @@ const routes = [
     name: 'Home',
     component: Home
   },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
 ]
 
 const router = new VueRouter({
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,69 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="card">
+      <div class="card-body">
+        <h2 class="card-title">ToDo List</h2>
+        <input type="text" v-model="newItem" class="mb-4">
+        <button @click="postItem" class="btn btn-outline-primary btn-sm ml-5">追加</button>
+        <div v-for="(data,index) in items" :key="index">
+          <input type="text" v-model="data.item" class="mb-2">
+          <button @click="putItem(data.id, data.item)" class="btn btn-outline-warning btn-sm ml-5">更新</button>
+          <button @click="deleteItem(data.id)" class="btn btn-outline-danger btn-sm ml-2">削除</button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
+import axios from "axios";
 
 export default {
+  data() {
+    return {
+      newItem: "",
+      items: []
+    }
+  },
+  methods: {
+    async getItems() {
+      const toDoItems = await axios.get("http://127.0.0.1:8000/api/todo_item");
+      this.items = toDoItems.data.data;
+    },
+    async postItem() {
+      const toDoItem = {item: this.newItem};
+      await axios.post("http://127.0.0.1:8000/api/todo_item", toDoItem);
+      await this.getItems();
+    },
+    async putItem(id, item) {
+      const toDoItem = {item: item};
+      await axios.put("http://127.0.0.1:8000/api/todo_item/" + id, toDoItem);
+    },
+    async deleteItem(id) {
+      await axios.delete("http://127.0.0.1:8000/api/todo_item/" + id);
+      await this.getItems();
+    }
+  },
+  created() {
+    this.getItems();
+  },
   name: 'Home',
   components: {
-    HelloWorld
+    // HelloWorld
   }
 }
 </script>
+
+<style>
+  .home {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    min-height: 100vh;
+  }
+  .card {
+    width: 70%;
+  }
+</style>
